cac00ad026e161913d873396befb2aa90fbf9f41
DudeFromKorea <peteryu24@naver.com>
2024-01-15 11:43:58 +0900

    Update WaterMillController.java

--- src/main/java/waterMill/WaterMillController.java
+++ src/main/java/waterMill/WaterMillController.java
@@ -3,6 +3,9 @@ package waterMill;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +16,22 @@ public class WaterMillController {
 	@Autowired
 	WaterMillService waterMillService;
 
+	@Autowired
+	private WaterMillResult waterMillResult;
+
 	@RequestMapping(value = "/setNum")
 	public WaterMillResult setForCalculation(@Valid int monthlyAmount, int endPeriod) {
 		return waterMillService.setForCalculation(monthlyAmount, endPeriod);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public WaterMillResult IllegalArgumentException(IllegalArgumentException exception) {
+		return waterMillResult.resultError("IllegalArgumentException",
+				ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage()));
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	public WaterMillResult handleRuntimeException(RuntimeException exception) {
+		return waterMillResult.resultError("RuntimeException", ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage()));
 	}
 }
