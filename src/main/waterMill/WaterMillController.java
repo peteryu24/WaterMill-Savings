a2358b01963c3dfb29f33cb403edc3f22e871780
DudeFromKorea <peteryu24@naver.com>
2024-01-15 11:37:07 +0900

    Delete src/main/waterMill directory

--- src/main/waterMill/WaterMillController.java
+++
@@ -1,8 +0,0 @@
-package waterMill;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class WaterMillController {
-
-}
--- src/main/waterMill/WaterMillService.java
+++
@@ -1,8 +0,0 @@
-package waterMill;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class WaterMillService {
-
-}
